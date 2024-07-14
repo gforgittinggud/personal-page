7ba1f2755b9fdf08121160dc16c1d0e505ad5f4b
Gordon <107980409+gordon23456@users.noreply.github.com>
2024-07-14 15:16:55 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,103 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open"); // appends the open word to the end of the class name, making it a subclass
+  icon.classList.toggle("open");
+}
+
+function toggleList() {
+  const elements = document.querySelectorAll("#skills, #skills-list");
+
+  elements.forEach((element) => {
+    if (element.classList.contains("hidden")) {
+      element.classList.remove("hidden");
+    } else {
+      element.classList.add("hidden");
+    }
+  });
+
+  const button = document.getElementById("buttonToggleList");
+  button.textContent =
+    button.textContent === "List View" ? "Tree View" : "List View";
+}
+
+function sendEmail() {
+  event.preventDefault();
+  var submitted = document.getElementById("submitted");
+  if (submitted.textContent == "submitted") {
+    alert("You have already submitted the Form.");
+    return;
+  }
+  var msg = document.getElementById("error-message");
+  if (msg === null) {
+    msg.textContent = "";
+  }
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var text = document.getElementById("text").value;
+
+  if (!name || !email || !text) {
+    document.getElementById("error-message").textContent =
+      "All fields are required.";
+    return;
+  }
+
+
+  axios
+    .post(`https://18.143.177.192/send-email`, {
+      name: name,
+      email: email,
+      text: text,
+    })
+    .then((response) => {
+      console.log(response.data);
+      alert("Form submitted successfully!");
+      document.getElementById("submitted").textContent = "submitted";
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("An issue occured on the server, Please try again.");
+    });
+}
+
+// document
+//   .getElementById("contact-form")
+//   .addEventListener("submit", function (event) {
+//     var submitted = document.getElementById("submitted");
+//     if (submitted.textContent == "submitted") {
+//       alert("You have already submitted the Form.");
+//       return;
+//     }
+
+//     event.preventDefault();
+
+//     var msg = document.getElementById("error-message");
+//     if (msg === null) {
+//       msg.textContent = "";
+//     }
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("email").value;
+//     var message = document.getElementById("comment").value;
+
+//     if (!name || !email || !message) {
+//       document.getElementById("error-message").textContent =
+//         "All fields are required.";
+//       return;
+//     }
+
+//   });
+
+// function checkScreenSize() {
+//     const width = window.innerWidth;
+//     const height = window.innerHeight;
+
+//     const desiredWidth = 850;
+//     const desiredHeight = 800;
+
+//     if (width <= desiredWidth || height <= desiredHeight) {
+
+//     }
+// }
+
+// window.onload = checkScreenSize;
+// window.addEventListener('resize', checkScreenSize);
